d3a313986063029ec1111db269489ac8d7d7b55d
Raphael Amorim <rapha850@gmail.com>
2014-11-21 16:07:37 -0200

    return data in game

--- game/views.py
+++ game/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
+
+from pprint import pprint
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to GameData Api v1.")
+    return HttpResponse("Welcome to GeekData Api v1. Check docs to use Game API")
 
 def game(request):
-    return HttpResponse("Game")
+	data = json.loads(open("./data/game/data.json").read())
+	res  = json.dumps(data)
+
+	return HttpResponse(res)
 
 def user(request):
     return HttpResponse("User")
