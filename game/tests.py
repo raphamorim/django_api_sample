5c36cd3b2274b25ede28d04713de9c9ef5e696c5
Raphael Amorim <rapha850@gmail.com>
2014-11-26 12:53:49 -0500

    created sample test

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
 from django.test import TestCase
 
 # Create your tests here.
+
+def test_get_list(self):
+    # 200 if list exists
+    response = self.client.get('127.0.0.1:8000/game/')
+    self.assertEqual(response.status_code, 200)
