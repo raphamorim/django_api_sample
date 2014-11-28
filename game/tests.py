604d4202e37baf45f7034c70de51dd90799332e0
Raphael Amorim <rapha850@gmail.com>
2014-11-28 01:29:06 -0200

    trying something new on test case for routes

--- game/tests.py
+++ game/tests.py
@@ -2,9 +2,20 @@
 
 from django.test import TestCase
 
+# from django.utils import unittest
+
 # Create your tests here.
 
-def test_get_list(self):
-    # 200 if list exists
-    response = self.client.get('127.0.0.1:8000/game/')
-    self.assertEqual(response.status_code, 200)
+class RoutesTest(TestCase):
+
+    def test_get_all_games_data(self):
+        # 200 if route exists
+        response = self.client.get('/game/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_game_by_especified_category(self):
+        # 200 if game exists
+        response = self.client.get('/game/category/')
+        self.assertEqual(response.status_code, 200)
+
+        # 404 if game not exists
